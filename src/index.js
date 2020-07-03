164023c6cc8185fa0fae4cfdcfde2e83ab72e85f
vinh bao thuong <vinhbaothuong@vietbando.vn>
2020-07-03 16:15:03 +0700

    change style

--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from "@material-ui/styles";
 import { CssBaseline } from "@material-ui/core";
-import themes from './themes/themes'
+import themes from './themes';
 import App from "./components/App";
 import * as serviceWorker from './serviceWorker';
-
+import { LayoutProvider } from "./context/LayoutContext";
+import { UserProvider } from "./context/UserContext";
 ReactDOM.render(
-  <React.StrictMode>
-    <ThemeProvider theme={themes.default}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
+  <LayoutProvider>
+    <UserProvider>
+      <React.StrictMode>
+        <ThemeProvider theme={themes.default}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </React.StrictMode>
+    </UserProvider>
+  </LayoutProvider>,
   document.getElementById('root')
 );
 serviceWorker.unregister();
--- src/themes/themes.js
+++ src/themes/themes.js
@@ -3,5 +3,67 @@ const themes = createMuiTheme({
     palette: {
         primary: { 500: '#467fcf' },
     },
+    customShadows: {
+        widget:
+            "0px 3px 11px 0px #E8EAFC, 0 3px 3px -2px #B2B2B21A, 0 1px 8px 0 #9A9A9A1A",
+        widgetDark:
+            "0px 3px 18px 0px #4558A3B3, 0 3px 3px -2px #B2B2B21A, 0 1px 8px 0 #9A9A9A1A",
+        widgetWide:
+            "0px 12px 33px 0px #E8EAFC, 0 3px 3px -2px #B2B2B21A, 0 1px 8px 0 #9A9A9A1A",
+    },
+    overrides: {
+        MuiBackdrop: {
+            root: {
+                backgroundColor: "#4A4A4A1A",
+            },
+        },
+        MuiMenu: {
+            paper: {
+                boxShadow:
+                    "0px 3px 11px 0px #E8EAFC, 0 3px 3px -2px #B2B2B21A, 0 1px 8px 0 #9A9A9A1A",
+            },
+        },
+        MuiSelect: {
+            icon: {
+                color: "#B9B9B9",
+            },
+        },
+        MuiListItem: {
+            root: {
+                "&$selected": {
+                    backgroundColor: "#F3F5FF !important",
+                    "&:focus": {
+                        backgroundColor: "#F3F5FF",
+                    },
+                },
+            },
+            button: {
+                "&:hover, &:focus": {
+                    backgroundColor: "#F3F5FF",
+                },
+            },
+        },
+        MuiTouchRipple: {
+            child: {
+                backgroundColor: "white",
+            },
+        },
+        MuiTableRow: {
+            root: {
+                height: 56,
+            },
+        },
+        MuiTableCell: {
+            root: {
+                borderBottom: "1px solid rgba(224, 224, 224, .5)",
+            },
+            head: {
+                fontSize: "0.95rem",
+            },
+            body: {
+                fontSize: "0.95rem",
+            },
+        },
+    },
 })
 export default themes
